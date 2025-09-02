5ad8539923b7b69232dc4dd3371acaa35aa82987
Iago Lebron <iagoamorim057@gmail.com>
2025-09-02 00:35:00 +0000

    aula 01/09/2025

---
+++ src/main/java/app/Tarefa.java
@@ -0,0 +1,34 @@
+package app;
+
+public class Tarefa {
+    private long id;
+    private String descricao;
+    private boolean isConcluido; // atributos booleanos tem como prefixos "is"
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluido() { // getter
+        return isConcluido;
+    }
+    public void setConcluido(boolean isConcluido) { // setter
+        this.isConcluido = isConcluido;
+    }
+
+    public Tarefa(long id, String descricao, boolean isConcluido) {
+        this.setId(id);
+        this.setDescricao(descricao);
+        this.setConcluido(isConcluido);
+    }
+}
